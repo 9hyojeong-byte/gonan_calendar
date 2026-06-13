70d0c4b4eb74d31dbc460893eec7cdc6cdab4627
9hyojeong-byte <9.hyojeong@gmail.com>
2026-06-13 20:45:33 +0900

    서비스명 교체, 아이콘 교체테스트

--- vite.config.js
+++ vite.config.js
@@ -19,9 +19,9 @@ export default defineConfig({
         orientation: 'portrait',
         start_url: '/',
         icons: [
-          { src: 'icons/icon-192.svg', sizes: '192x192', type: 'image/svg+xml' },
-          { src: 'icons/icon-512.svg', sizes: '512x512', type: 'image/svg+xml' },
-          { src: 'icons/icon-512.svg', sizes: '512x512', type: 'image/svg+xml', purpose: 'maskable' },
+          { src: 'icons/icon_toto_margin.png', sizes: '192x192', type: 'image/svg+xml' },
+          { src: 'icons/icon_toto_margin.png', sizes: '512x512', type: 'image/svg+xml' },
+          { src: 'icons/icon_toto_margin.png', sizes: '512x512', type: 'image/svg+xml', purpose: 'maskable' },
         ],
       },
       workbox: {
